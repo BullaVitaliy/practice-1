2e1f8c2e1cf40715f11b9ce90607503e4ca49dd5
Vitaliy <bullavitaliy@gmil.com>
2022-01-13 19:40:04 +0200

    practice1/lesson1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// practice1
+
+//1
+const numberOfFilms = +prompt('Скільки фільмів ви вже переглянуди?', '');
+
+
+//2
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+//3
+let a = prompt('Один із останіх фільмів?', ''),
+    b = prompt('На скільки оцінете його?', ''),
+    c = prompt('Один із останіх фільмів?', ''),
+    d = prompt('На скільки оцінете його?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
