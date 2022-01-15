7a06769952597289d8c3f291b9216211d6239670
Vitaliy <bullavitaliy@gmil.com>
2022-01-15 04:00:26 +0200

    practice2

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // practice-1
 
-//1
-const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+//1----------------перше питання до користувача
+const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', ''); //+promot перетворює пустий рядок в 0
+// ок = вертає '', скасувати = вертає null
 
-
-//2
+//2----------------умовна база даних
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -13,13 +13,63 @@ const personalMovieDB = {
     privat: false
 };
 
-//3
-let a = prompt('Один із останіх фільмів?', ''),
-    b = prompt('На скільки оцінете його?', ''),
-    c = prompt('Один із останіх фільмів?', ''),
-    d = prompt('На скільки оцінете його?', '');
+//3----------------питання до глядача---цикл-умова
+//-----for(;;){}
+for (let i = 0; i < 2; i++){
+    const a = prompt('Один із останіх фільмів?', ''),
+          b = prompt('На скільки оцінете його?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        console.log('error');
+        i--;//віднімає одну ітерацію якщо не виповнились всі умови
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//-----while(){}
+// let i = 0;
+// while (i < 2) {
+//     const a = prompt('Один із останіх фільмів?', ''),
+//           b = prompt('На скільки оцінете його?', '');
 
-console.log(personalMovieDB);
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log('error');
+//         i--;//віднімає одну ітерацію якщо не виповнились всі умови
+//         }
+
+//     i++;
+// }
+
+
+//-----do{}while()
+// let i = 0;
+// do {
+//     const a = prompt('Один із останіх фільмів?', ''),
+//           b = prompt('На скільки оцінете його?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     } else {
+//         console.log('error');
+//         i--;//віднімає одну ітерацію якщо не виповнились всі умови
+//         }
+    
+//         i++;
+// } while (i < 2);
+
+//4------------------оцінка глядача
+if(personalMovieDB.count >= 30) {
+    alert('Ви кіноман');
+} else if(personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+    alert('Ви класний глядач!');
+} else if(personalMovieDB.count < 10) { //&& personalMovieDB.count >= 1
+    alert('Переглянуто досить мало фільмів');
+} else {
+    alert('Відбулась помилка');
+}
+
+console.log(personalMovieDB);//виводимо в консоль умовну базу даних(об'єкт)
+    
